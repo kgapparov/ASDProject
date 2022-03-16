d082ffcd7d9334ab276351e5546e0e58cd918d90
khassangali <hasangaly@gmail.com>
2022-03-16 17:03:40 -0500

    cosmetic fix

--- src/main/java/Internal/bank/Application.java
+++ src/main/java/Internal/bank/Application.java
@@ -1,20 +1,13 @@
 package Internal.bank;
 
 import Internal.bank.ui.BankApplicationForm;
-import Internal.framework.controller.AccountServiceApplicationFactory;
-import Internal.framework.controller.EnvironmentType;
-import Internal.framework.module.commands.CommandInterface;
 import Internal.framework.ui.ApplicationFrm;
-
-import javax.swing.*;
 
 public class Application {
     public static void main(String[] args) {
         BankServiceAplication application = new BankServiceAplication();
         ApplicationFrm form = new BankApplicationForm();
         application.run(application, form);
-
-
     }
 
 }
