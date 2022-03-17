757b26760b2ca23b430ac394cfd8b34805733fde
MahmoudAnwar613725 <mahmohamed@miu.edu>
2022-03-17 11:09:05 -0500

    merge with master

--- src/main/java/Internal/creditcard/controller/Application.java
+++ src/main/java/Internal/creditcard/controller/Application.java
@@ -3,12 +3,12 @@ package Internal.creditcard.controller;
 import Internal.creditcard.ui.CardFrm;
 import Internal.framework.controller.AccountServiceApplicationFactory;
 import Internal.framework.controller.EnvironmentType;
-import Internal.framework.module.EmailSender;
+import Internal.framework.module.BankEmailSender;
 
 public class Application {
     public static void main(String[] args) {
         AccountServiceApplicationFactory application = new CreditCardApplication();
-        application.registerObserver(new EmailSender());
+        application.registerObserver(new BankEmailSender());
         CardFrm form = new CardFrm();
         application.run(application, form);
     }
