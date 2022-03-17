f57be130cb5a206187f0341c3375dc1d5fa790f4
Altangerel Ganbaatar <mr.tuulai@gmail.com>
2022-03-17 10:43:58 -0500

    Updated Observer and classDiagram

--- src/main/java/Internal/bank/Application.java
+++ src/main/java/Internal/bank/Application.java
@@ -1,21 +1,13 @@
 package Internal.bank;
 
 import Internal.bank.ui.BankApplicationForm;
-import Internal.creditcard.CreditCardApplication;
-import Internal.framework.controller.AccountServiceApplicationFactory;
-import Internal.framework.controller.EnvironmentType;
-import Internal.framework.module.CreditCardEmailSender;
-import Internal.framework.module.EmailSender;
-import Internal.framework.module.commands.CommandInterface;
+import Internal.framework.module.BankEmailSender;
 import Internal.framework.ui.ApplicationFrm;
-import Internal.ui.ccard.CardFrm;
-
-import javax.swing.*;
 
 public class Application {
     public static void main(String[] args) {
         BankServiceAplication application = new BankServiceAplication();
-        application.registerObserver(new EmailSender());
+        application.registerObserver(new BankEmailSender());
         ApplicationFrm form = new BankApplicationForm();
         application.run(application, form);
     }
--- src/main/java/Internal/framework/module/CreditCardEmailSender.java
+++ src/main/java/Internal/framework/module/CreditCardEmailSender.java
@@ -7,13 +7,16 @@ public class CreditCardEmailSender implements Observer{
     @Override
     public void update(Account account) {
         AccountEntry entry = account.getEntryList().get(account.getEntryList().size() - 1);
-        System.out.println(entry.getAmount());
         if (account.getCustomer().getCustomerType() == CustomerType.COMPANY) {
-            System.out.println("{Credit Card} Sending an email to COMPANY - " + account.getCustomer().getClientName());
+            System.out.println("{Credit Card} Sending an email to COMPANY - " + account.getCustomer().getClientName()
+                    + " " + entry.getDescription() + " " + entry.getAmount());
         } else if (account.getCustomer().getCustomerType() == CustomerType.INDIVIDUAL && (
             entry.getAmount() >= 400 || account.getBalance() < 0)) {
-            System.out.println("{Credit Card} Sending an email to INDIVIDUAL - " + account.getCustomer().getClientName() + " "
-                    + account.getOperationName());
+            System.out.println("{Credit Card} Sending an email to INDIVIDUAL - " + account.getCustomer().getClientName()
+                    + " " + entry.getDescription() + " " + entry.getAmount());
+        } else if (entry.getAmount() <= -400) {
+            System.out.println("{Credit Card} Sending an email to INDIVIDUAL - " + account.getCustomer().getClientName()
+                    + " " + entry.getDescription() + " " + entry.getAmount());
         }
     }
 }
