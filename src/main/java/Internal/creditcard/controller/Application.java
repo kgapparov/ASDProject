ec8387ad2da9ae14862c12faf4f4842cefef4350
Altangerel Ganbaatar <mr.tuulai@gmail.com>
2022-03-17 11:26:58 -0500

    Changed CreditCard Observer with CreditCardEmailSender

--- src/main/java/Internal/creditcard/controller/Application.java
+++ src/main/java/Internal/creditcard/controller/Application.java
@@ -3,12 +3,12 @@ package Internal.creditcard.controller;
 import Internal.creditcard.ui.CardFrm;
 import Internal.framework.controller.AccountServiceApplicationFactory;
 import Internal.framework.controller.EnvironmentType;
-import Internal.framework.module.BankEmailSender;
+import Internal.framework.module.CreditCardEmailSender;
 
 public class Application {
     public static void main(String[] args) {
         AccountServiceApplicationFactory application = new CreditCardApplication();
-        application.registerObserver(new BankEmailSender());
+        application.registerObserver(new CreditCardEmailSender());
         CardFrm form = new CardFrm();
         application.run(application, form);
     }
