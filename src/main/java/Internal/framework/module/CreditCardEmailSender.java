817c8a8825d16c4b2927166e94bb738fb8777ace
Altangerel Ganbaatar <mr.tuulai@gmail.com>
2022-03-16 20:52:18 -0500

    Updated Observer

--- src/main/java/Internal/framework/module/CreditCardEmailSender.java
+++ src/main/java/Internal/framework/module/CreditCardEmailSender.java
@@ -2,20 +2,18 @@ package Internal.framework.module;
 
 public class CreditCardEmailSender implements Observer{
 
-    public CreditCardEmailSender() {
-      }
+    public CreditCardEmailSender() {}
 
     @Override
     public void update(Account account) {
         AccountEntry entry = account.getEntryList().get(account.getEntryList().size() - 1);
         System.out.println(entry.getAmount());
         if (account.getCustomer().getCustomerType() == CustomerType.COMPANY) {
-            System.out.println("Sending an email to - " + account.getCustomer().getClientName() + " "
-                    + account.getCustomer().getCustomerType());
+            System.out.println("{Credit Card} Sending an email to COMPANY - " + account.getCustomer().getClientName());
         } else if (account.getCustomer().getCustomerType() == CustomerType.INDIVIDUAL && (
             entry.getAmount() >= 400 || account.getBalance() < 0)) {
-            System.out.println("Sending an email to - " + account.getCustomer().getClientName() + " "
-                    + account.getCustomer().getCustomerType() + " " + account.getOperationName());
+            System.out.println("{Credit Card} Sending an email to INDIVIDUAL - " + account.getCustomer().getClientName() + " "
+                    + account.getOperationName());
         }
     }
 }
--- src/main/java/Internal/framework/module/EmailSender.java
+++ src/main/java/Internal/framework/module/EmailSender.java
@@ -2,20 +2,18 @@ package Internal.framework.module;
 
 public class EmailSender implements Observer{
 
-    public EmailSender() {
-      }
+    public EmailSender() {}
 
     @Override
     public void update(Account account) {
         AccountEntry entry = account.getEntryList().get(account.getEntryList().size() - 1);
         System.out.println(entry.getAmount());
         if (account.getCustomer().getCustomerType() == CustomerType.COMPANY) {
-            System.out.println("Sending an email to - " + account.getCustomer().getClientName() + " "
-                    + account.getCustomer().getCustomerType());
+            System.out.println("{Banking} Sending an email to COMPANY - " + account.getCustomer().getClientName());
         } else if (account.getCustomer().getCustomerType() == CustomerType.INDIVIDUAL && (
             entry.getAmount() >= 500 || account.getBalance() < 0)) {
-            System.out.println("Sending an email to - " + account.getCustomer().getClientName() + " "
-                    + account.getCustomer().getCustomerType() + " " + account.getOperationName());
+            System.out.println("{Banking} Sending an email to INDIVIDUAL - " + account.getCustomer().getClientName() + " "
+                    + account.getOperationName());
         }
     }
 }
